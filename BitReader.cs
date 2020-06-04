ee6a8f5cf439d839702c09ba094a618c21464eae
Yaroslav <pablo1231435@gmail.com>
2020-06-04 19:48:58 +0300

    first commit

---
+++ BitReader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab4Bits
+{
+    class BitReader : IDisposable
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
